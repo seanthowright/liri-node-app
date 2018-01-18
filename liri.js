c61cbe025247715e461ab6c5a1bf8a945a52a753
Sean Wright <seanthowright@gmail.com>
2018-01-18 17:32:57 -0500

    Spotify/Twitter now work through node, added log.txt, clean up

--- liri.js
+++ liri.js
@@ -1,1 +1,44 @@
 require("dotenv").config();
+
+//Node, NPM, Keys
+
+let keys = require('./keys.js');
+let request = require("request");
+let fs = require("fs");
+let argOne = process.argv[2];
+let argTwo = process.argv[3];
+
+// Auth for Spotify and Twitter
+
+let twitter = require('twitter');
+let spotify = require('node-spotify-api');
+let spotifyKey = new spotify(keys.spotify);
+let twitterKey = new twitter(keys.twitter);
+
+// Twitter function
+
+if (argOne === "my-tweets") {
+
+    let params = { screen_name: "yohanguthrie", count: 20 };
+
+    twitterKey.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+        for (var i = 0; i < tweets.length; i++) {
+            console.log("\n----------------");
+            console.log("Created on " + tweets[i].created_at);
+            console.log(tweets[i].text);
+
+        }
+    });
+}
+
+if (argOne === "spotify-this-song") {
+
+    spotifyKey.search({ type: 'track', query: argTwo, limit: 1 }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+}
